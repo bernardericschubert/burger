ca60e24b0d062d729a2a019c2aa7f40f4e3164e5
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-09-15 18:05:47 -0700

    MVC file structure, completed DB files

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('Awesome Burger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Awesome Burger Deluxe');
+
+INSERT INTO burgers(burger_name)
+VALUES('Super Awesome Cheeseburger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Super Awesome Cheeseburger Deluxe');
+
+INSERT INTO burgers(burger_name)
+VALUES('Impossible Burger');
