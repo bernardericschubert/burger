dd0e8fbb2d90b1c493fa87d54c9adf43d3c1682d
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-09-15 18:15:49 -0700

    base connection completed

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "bootcamp",
+  database: "burgers_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+// Export Connection
+module.exports = connection;
