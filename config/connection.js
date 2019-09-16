7806e34ada14b85870a27615b31f619100324451
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-09-15 20:04:54 -0700

    fixed typo in connect function

--- config/connection.js
+++ config/connection.js
@@ -18,8 +18,6 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
 });
 
 // Export Connection
