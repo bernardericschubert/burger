9435e8d4ff5288c20b89780c9a8c4a11a873dacd
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-09-15 20:01:03 -0700

    fixed typo

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("App now listening at http://localhost:" + PORT);
+  console.log("App is now listening at http://localhost:" + PORT);
 });
